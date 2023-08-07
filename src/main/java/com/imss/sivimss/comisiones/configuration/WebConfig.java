65dc01db6ab8a53aee79a8749bbe764d4cea7e84
LeandroPeopleMedia <107154425+LeandroPeopleMedia@users.noreply.github.com>
2023-08-07 11:45:23 -0600

    cahneg package

--- src/main/java/com/imss/sivimss/comisiones/configuration/WebConfig.java
+++ src/main/java/com/imss/sivimss/comisiones/configuration/WebConfig.java
@@ -1,14 +1,9 @@
-package com.imss.sivimss.arquetipo.configuration;
+package com.imss.sivimss.comisiones.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Clase para habilitar los cors (las peticiones desde un cliente como react, vue, angular)
- * @author pnolasco
- *
- */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
